5254e11ec87be24e224d382798783cebce5f3c63
Adewah245 <adewahjames@gmail.com>
2026-07-25 15:15:31 +0100

    first database

---
+++ doc/database.md
@@ -0,0 +1,20 @@
+# AgriLinks Database
+
+## Table: lgas
+
+Purpose:
+Stores all Idoma Local Government Areas.
+
+Columns
+
+id
+Primary key
+
+code
+Short code
+
+name
+Official LGA name
+
+created_at
+Creation timestamp
---
+++ migrations/006_seed_lgas.sql
@@ -0,0 +1,11 @@
+insert into lgas (code, name)
+values
+('ADO', 'Ado'),
+('AGA', 'Agatu'),
+('APA', 'Apa'),
+('OBI', 'Obi'),
+('OGB', 'Ogbadibo'),
+('OHI', 'Ohimini'),
+('OKP', 'Okpoku'),
+('OTK', 'Otukpo'),
+('OJU', 'Oju')
